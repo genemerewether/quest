3113ad75f3154e1c96836860807876d26f577e6d
Gene Merewether <mereweth@jpl.nasa.gov>
2019-06-05 10:45:43 -0700

    add sysid scripts to run sequence combos

--- scripts/sysid/takeoffprep_land.bash
+++ scripts/sysid/takeoffprep_land.bash
@@ -7,8 +7,6 @@ rosrun quest run_seq_client \
        /seq/vislam_on.bin \
        /seq/arm.bin \
        \
-       /seq/linvel_box.bin \
-       \
        /seq/negthrust.bin \
        /seq/attrate.bin \
        /seq/vislam_off.bin \
--- scripts/sysid/vel_updown.bash
+++ scripts/sysid/vel_updown.bash
@@ -5,11 +5,13 @@ rosrun quest run_seq_client \
        /seq/negthrust.bin \
        /seq/attrate.bin \
        /seq/vislam_on.bin \
+       /seq/log_on.bin \
        /seq/arm.bin \
        \
-       /seq/linvel_box.bin \
+       /seq/linvel_updown.bin \
        \
        /seq/negthrust.bin \
        /seq/attrate.bin \
+       /seq/log_off.bin \
        /seq/vislam_off.bin \
        /seq/disarm.bin
